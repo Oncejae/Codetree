8989f0aa2b157027ed66a60167a08e4704b1dc4c
Oncelee <imop1289@naver.com>
2025-11-29 17:36:53 +0900

    Title: 특정 수 도달하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251129/특정 수 도달하기/reaching-specific-number.c
+++ 251129/특정 수 도달하기/reaching-specific-number.c
@@ -3,20 +3,19 @@
 int main() {
     int a[10];
     int sum=0;
-    int b;
+    int count =0;
     float avg;
     for(int i=0; i<10; i++){
         scanf("%d", &a[i]);
         if(a[i]<250){
             sum += a[i];
+            count++;
         }
         if(a[i]>=250){
-            b = i;
             break;
         }
-        b= i;
     }
-    avg = (float)sum / b;
+    avg = (float)sum / count;
     printf("%d %.1f", sum, avg);    
 
     return 0;
