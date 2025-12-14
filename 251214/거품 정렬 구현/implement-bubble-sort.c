898ef3d298eae8d1b4dba1502f09ed4597b19a2b
Oncelee <imop1289@naver.com>
2025-12-14 15:02:54 +0900

    Title: 거품 정렬 구현, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251214/거품 정렬 구현/implement-bubble-sort.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    // Please write your code here.
+    int n;
+    scanf("%d", &n);
+    int arr[100];
+    for(int i=0; i<n; i++){
+    scanf("%d ", &arr[i]);
+    }
+    int temp;
+    for (int i=0; i<n; i++){
+        for(int j=0; j<n-i-1; j++){
+            if(arr[j]> arr[j+1]){
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    for(int i=0; i<n; i++){
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
